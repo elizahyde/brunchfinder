d21c10697fb7afbdc25837a0c99f2e2af8d8974e
elizahyde <elizabeth@elizahyde.com>
2013-06-17 16:39:37 -0700

    made sticky nav removed foundation

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,11 +6,7 @@ def get_brunch
     client = Yelp::Client.new
     request = Yelp::V2::Search::Request::Location.new(
                 :category_filter => "breakfast_brunch"
-                :term => ["bloody mary", "bloody marys", "bloody maries"]
-                # Location Specifies the combination of "address,
-                # neighborhood, city, state or zip, optional
-                # country" to be used when searching for businesses.
-                :location => "Hayes",
+                :location => location,
                 :consumer_key => YELPCONKEY,
                 :consumer_secret => YELPCONSECRET,
                 :token => YELPTOKEN,
