1d620d27d8156349af8f726fb506b78274ed22ef
elizahyde <elizabeth@elizahyde.com>
2013-06-29 01:51:43 -0700

    Finished frontend, added tooltip WIP BOOM

--- app/models/search.rb
+++ app/models/search.rb
@@ -19,35 +19,7 @@ class Search
       }
 
       results[k] = results[k].merge(img_results)
-      # binding.pry
       end
       return results
     end
-
-    # Making a Dramatic Business Decision to Kill cravings options
-  # def self.get_bloodies(latlng)
-  #   response = HTTParty.get("https://api.foursquare.com/v2/venues/explore?ll=#{latlng}&query=brunch,bloody+mary&client_id=#{ENV['FOURSQCLIENTID']}&client_secret=#{ENV['FOURSQCLIENTSECRET']}")
-  #   results = {}
-
-  #   response["response"]["groups"][0]["items"].each do |f|
-  #     results[f["venue"]["name"]] = {fid:f["venue"]["id"], furl:f["venue"]["canonicalUrl"], faddress:f["venue"]["location"]["address"], fcity:f["venue"]["location"]["city"],
-  #       totalCheckins:f["venue"]["stats"]["checkinsCount"], flat:f["venue"]["location"]["lat"], flng:f["venue"]["location"]["lng"],
-  #       flikes_count:f["venue"]["likes"]["count"], ftips_count:f["venue"]["stats"]["tipCount"],
-  #       fhereNow:f["venue"]["hereNow"]["count"],fphotos_count:f["venue"]["photos"]["count"]}
-  #   end
-  # return results
-  # end
-
-  # def self.get_mimosas(latlng)
-  #   response = HTTParty.get("https://api.foursquare.com/v2/venues/explore?ll=#{latlng}&query=brunch,mimosa&client_id=#{ENV['FOURSQCLIENTID']}&client_secret=#{ENV['FOURSQCLIENTSECRET']}")
-  #   results = {}
-
-  #   response["response"]["groups"][0]["items"].each do |f|
-  #     results[f["venue"]["name"]] = {fid:f["venue"]["id"], furl:f["venue"]["canonicalUrl"], faddress:f["venue"]["location"]["address"], fcity:f["venue"]["location"]["city"],
-  #       totalCheckins:f["venue"]["stats"]["checkinsCount"], flat:f["venue"]["location"]["lat"], flng:f["venue"]["location"]["lng"],
-  #       flikes_count:f["venue"]["likes"]["count"], ftips_count:f["venue"]["stats"]["tipCount"],
-  #       fhereNow:f["venue"]["hereNow"]["count"],fphotos_count:f["venue"]["photos"]["count"]}
-  #   end
-  # return results
-  # end
 end
