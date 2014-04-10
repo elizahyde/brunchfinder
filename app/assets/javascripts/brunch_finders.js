807247fc46c8bc2facac9494f7eebf67f44ed4b8
elizahyde <elizabeth@elizahyde.com>
2014-04-09 17:44:57 -0700

    moved results JS to brunch_finders.js, added comments

--- app/assets/javascripts/brunch_finders.js
+++ app/assets/javascripts/brunch_finders.js
@@ -10,4 +10,43 @@ $(function() {
   $("submit").click(function(){
     $("#geolocale").trigger("geocode");
   });
+
+  // Results Page
+  var $container = $('#container');
+  $("#container");
+  $container.isotope({
+    itemSelector: '.venue',
+    layoutMode : 'masonry',
+    sortBy: 'totalCheckins',
+    sortAscending : false,
+    animationEngine : 'best-available',
+    animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false },
+    getSortData : {
+      distance : function ( $elem ) {
+        return parseFloat($elem.find('.distance').text() );
+      },
+      hereNow : function ( $elem ) {
+        return parseInt($elem.find('.hereNow').text(), 10 );
+      },
+      totalCheckins : function ( $elem ) {
+        return parseInt($elem.find('.totalCheckins').text(), 10 );
+      }
+    }
+  });
+
+  $('#sortBy a').click(function(){
+    // get href attribute, minus the '#'
+    var sortName = $(this).attr('href').slice(1);
+    $('#container').isotope({ sortBy : sortName });
+    return false;
+  });
+
+  // Unused for now
+  $('#hereNow').tooltip({
+    placement: 'left',
+    content: 'Current Checkins'
+  });
 });
